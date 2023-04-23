8d53f50046596e057d3e4d8a44c1cb704c5c117d
liuruijun <ruijun.liu-a1654@aqara.com>
2023-04-23 10:02:15 +0800

    quick-sort 3

--- quick-sort/index.js
+++ quick-sort/index.js
@@ -25,7 +25,7 @@ function quickSort(arr) {
  * https://blog.csdn.net/m0_61003706/article/details/119790406
  * https://www.conardli.top/docs/algorithm/%E6%8E%92%E5%BA%8F/%E5%BF%AB%E9%80%9F%E6%8E%92%E5%BA%8F.html
  */
-function quickSortTwo(arr, start = 0, end = arr.length - 1) {
+function quickSort_v2(arr, start = 0, end = arr.length - 1) {
     if (end - start < 1) return
     let l = start
     let r = end
@@ -42,14 +42,35 @@ function quickSortTwo(arr, start = 0, end = arr.length - 1) {
     }
 
     arr[l] = target
-    quickSortTwo(arr, start, l - 1)
-    quickSortTwo(arr, l + 1, end)
+    quickSort_v2(arr, start, l - 1)
+    quickSort_v2(arr, l + 1, end)
+    return arr
+}
+/**
+ * 怎么理解呢~
+ */
+function quickSort_v3(arr, start = 0, end = arr.length - 1) {
+    while(start < end){
+        let l = start
+        let r = end
+        let target = arr[start]
+        while (l < r) {
+            while (l < r && arr[r] >= target) r--
+            if(l<r) arr[l++] = arr[r]
+            while (l < r && arr[l] < target) l++
+            if(l<r) arr[r--] = arr[l]
+        }
+        arr[l] = target
+        quickSort_v3(arr,l+1,r)
+        end = l - 1
+    }
     return arr
 }
 
 
 
 let result = quickSort([9, 4, 3, 1, 6, 3, 8, 7])
-let result2 = quickSortTwo([9, 4, 3, 1, 6, 3, 8, 7])
+let result2 = quickSort_v2([9, 4, 3, 1, 6, 3, 8, 7])
+let result3 = quickSort_v3([9, 4, 3, 1, 6, 3, 8, 7])
 
-console.log(result, result2);
+console.log(result, result2,result3);
