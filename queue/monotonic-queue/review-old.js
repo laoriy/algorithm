4c8e877e7220f2a8f9d3a5e3eb733232f7c71683
laoriy <1161612005@qq.com>
2024-03-13 12:09:58 +0800

    feat:  [复原 IP 地址]

--- queue/monotonic-queue/review-old.js
+++ queue/monotonic-queue/review-old.js
@@ -260,3 +260,74 @@ console.log(
     jump2([0]),
     jump2([1, 2])
 );
+
+
+/**
+ * 93. 复原 IP 地址
+ * 
+ * 有效 IP 地址 正好由四个整数（每个整数位于 0 到 255 之间组成，且不能含有前导 0），整数之间用 '.' 分隔。
+
+例如："0.1.2.201" 和 "192.168.1.1" 是 有效 IP 地址，但是 "0.011.255.245"、"192.168.1.312" 和 "192.168@1.1" 是 无效 IP 地址。
+给定一个只包含数字的字符串 s ，用以表示一个 IP 地址，返回所有可能的有效 IP 地址，这些地址可以通过在 s 中插入 '.' 来形成。你 不能 重新排序或删除 s 中的任何数字。你可以按 任何 顺序返回答案。
+
+ 
+
+示例 1：
+
+输入：s = "25525511135"
+输出：["255.255.11.135","255.255.111.35"]
+示例 2：
+
+输入：s = "0000"
+输出：["0.0.0.0"]
+示例 3：
+
+输入：s = "101023"
+输出：["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
+ */
+
+/**
+ * Generate all possible valid IP addresses from given string.
+ *
+ * @param {string} s - the input string
+ * @return {string[]} an array of all valid IP addresses
+ */
+function restoreIpAddresses(s) {
+    // pointId 属于 0 - 3
+    const result = []
+    const segments = new Array(4);
+    function dfs(segmentId, start) {
+        // 搜索到第四段了，如果这时候刚好也结束了，就加入结果数组
+        if (segmentId === 4) {
+            if (start === s.length) {
+                result.push(segments.join('.'))
+            }
+            return
+        }
+        if (start === s.length) return;
+
+        if (s[start] === '0') { // 这一位是0，
+            segments[segmentId] = 0
+            dfs(segmentId + 1, start + 1)
+            return
+        }
+        // 一般情况，枚举每一种可能性并递归
+        for (let i = start; i < s.length; i++) {
+            let v = Number(s.slice(start, i + 1))
+            if (v > 0 && v <= 255) {
+                segments[segmentId] = v;
+                dfs(segmentId + 1, i + 1)
+            } else {
+                break;
+            }
+        }
+
+    }
+    dfs(0, 0)
+    return result
+};
+
+console.log(
+    restoreIpAddresses('0000'),
+    restoreIpAddresses('25525511135'),
+)
